7e2a9922a46303951a94d71bcdefce3328a9dddf
Lrd134 <larryc3200@gmail.com>
2021-05-30 18:11:30 -0400

    commit tree 06118a0ba225f4762984d2595bfdb7eed9eea34f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,19 +13,30 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/signup" do
+
 		erb :signup
 	end
-
-	post "/signup" do
-		#your code here!
-	end
+  post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
+  end 
 
 	get "/login" do
 		erb :login
 	end
+  post "/login" do
+  user = User.find_by(:username => params[:username])
 
-	post "/login" do
-		#your code here!
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
 	end
 
 	get "/success" do
